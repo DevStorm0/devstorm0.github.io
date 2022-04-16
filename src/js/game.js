73c5a1d795b5bf98ba6dc3c9588179813f324572
DevStorm <84731668+DevStorm0@users.noreply.github.com>
2022-04-16 23:57:27 +0300

    :)

--- src/js/game.js
+++ src/js/game.js
@@ -1,5 +1,5 @@
 const urlParams = new URLSearchParams(window.location.search);
 if (urlParams.has('play') == false) { location.href="./games" } else {
-    urlParams.get('game')
-    document.getElementById('game').setAttribute('src', urlParams.get('game'))
+    document.getElementById('game').setAttribute('src', urlParams.get('game'));
+    document.getElementById('title1').innerText = urlParams.get('name');
 }
